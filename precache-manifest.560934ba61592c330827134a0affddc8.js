abbd73a764200e812ec1a7b84427320d14e2103a
Mustapha <muscoff2008@gmail.com>
2019-05-25 01:26:50 +0000

    Updates

---
+++ precache-manifest.560934ba61592c330827134a0affddc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ee08640c2c0638d943b67f9a60f7b7",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "e1930ab7da58a330cf78",
+    "url": "/mysite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mysite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e1930ab7da58a330cf78",
+    "url": "/mysite/static/js/main.8c74d067.chunk.js"
+  },
+  {
+    "revision": "69f460fd7b0f3581d72a",
+    "url": "/mysite/static/js/runtime~main.2f907dfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  }
+]);
